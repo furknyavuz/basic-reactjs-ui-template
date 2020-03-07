d91efb2c376f2ee02304ee1ad5f2e286d523124f
Furkan Yavuz <furkan@bigarmor.com>
2020-03-07 22:31:09 +0300

    init

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @description holds configuration constants
+ */
+
+export const Config = {
+  GRAPHQL_URL: "http://localhost:4001",
+  AUTH_URL: "https://auth-server-nodejs-template-st.herokuapp.com"
+};
